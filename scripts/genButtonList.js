6360d8d62f6aada6104b04fa56374dd576af9353
carrotvegniak <veggiesrmylyfe@gmail.com>
2016-04-15 16:30:01 -0500

    genButtonList now accepts input from an array

--- scripts/genButtonList.js
+++ scripts/genButtonList.js
@@ -1,17 +1,17 @@
-/** Will add list elements horizontally to the inputted list
- * List elements will be buttons from A to Z
+/** Will add list elements horizontally to the specified list
  * @param #addList
  *		The list element onto which the list items will be added
  * @param #listType
  *		What type of terms is this is a list of?
  *		ie. Dictionary terms, historical figures etc.
+ * @param #buttonValues
+ *		An array of values that the butons will display
+ *		and link to the search for
  */
- function genAlphaList( addList, listType )
+ function genButtonList( addList, listType, buttonValues )
  {
- 	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
  	// Add all the letters as buttons to the list
- 	for ( var i = 0; i < 26; i++ )
+ 	for ( var i = 0; i < buttonValues.length; i++ )
  	{
  		// Create the new list item
  		var entry = document.createElement( 'li' );
@@ -20,11 +20,11 @@
  		var newButton = document.createElement( 'button' );
 
  		// The method invocation to be run onclick as represented by a string
- 		var toCall = "chooseList( \"" + listType + "\", \"" + alphabet.charAt( i ) + "\" )";
+ 		var toCall = "chooseList( \"" + listType + "\", \"" + buttonValues[ i ] + "\" )";
 
  		// Modify the button to contain the letter and the correct term linkage
  		newButton.setAttribute( "onclick", toCall );
-        newButton.appendChild( document.createTextNode( alphabet.charAt( i ) ) );
+        newButton.appendChild( document.createTextNode( buttonValues[ i ] ) );
 
  		// Add the button to the list item
  		entry.appendChild( newButton );
@@ -34,7 +34,8 @@
  	}
  }
 
-/* Determines what set of terms this list will link to
+/**
+ * Determines what set of terms this list will link to
  * @param #listType
  *		The list element onto which the list items will be added
  * @param #searchLetter
