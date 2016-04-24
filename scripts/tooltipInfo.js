8db71a8600426a80b74bf73f1c542d311f58d59f
carrotvegniak <veggiesrmylyfe@gmail.com>
2016-04-24 00:50:29 -0500

    Testing global variable work-around

--- scripts/tooltipInfo.js
+++ scripts/tooltipInfo.js
@@ -1,3 +1,7 @@
+
+// The text to be read in from a file, split by line
+var text = [];
+
 /**
  * Modifies the content of the legislation map span
  * to contain the correct state's information
@@ -6,10 +10,6 @@
  */
  function tooltipInfo( state )
  {
- 	// Splits the text file into individual lines, storing
-	// these values in an array
-	var text = readTextFile( "scripts/Legislation_Info.txt" ).split( "\n" );
-
 	// Will store the information about that particular state
 	var info = "";
 
@@ -21,4 +21,11 @@
 
  	document.getElementById( "tooltipHeader" ).innerHTML = state;
  	document.getElementById( "tooltip" ).innerHTML = info;
+ }
+
+ function stateSetUp()
+ {
+ 	// Splits the text file into individual lines, storing
+	// these values in an array
+	text = readTextFile( "scripts/Legislation_Info.txt" ).split( "\n" );
  }
