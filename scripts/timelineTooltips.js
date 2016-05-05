78e84efe0a2b2ff945ada8bcc8890a098a388e03
carrotvegniak <veggiesrmylyfe@gmail.com>
2016-05-04 22:19:23 -0500

    Debugging

--- scripts/timelineTooltips.js
+++ scripts/timelineTooltips.js
@@ -2,8 +2,42 @@
 // History Page Timeline Hovering
 //
 
-// The tooltip currently being displayed
-var current = null;
+// All of the tooltips
+var tooltips = [];
+
+// Each tooltip has a heading and it's content
+function tooltip( heading, content )
+{
+    this.heading = heading;
+    this.content = content;
+}
+
+/**
+ * Initialize the tooltip values from the text file
+ * to the array {@see #tooltips}
+ */
+function loadTooltips()
+{
+    // An array of all the timeline tooltips' text
+    var tooltipText = readTextFile( "scripts/Timeline.txt" ).split( "\n" );
+
+    // Add all the tooltips to {@see #tooltips}
+    for ( var i = 2; i <= tooltipText.length;  i += 2 )
+    {
+        tooltips.push( new tooltip( tooltipText[ i - 2 ], tooltipText[ i - 1 ] ) );
+    }
+}
+
+
+// The tooltip currently being displayed, defaults to an
+// empty span element
+var current = document.createElement( "span" );
+
+// Set the class of the tooltip (for styling to take effect)
+current.setAttribute( "class", "tooltip" );
+
+// Add the tooltip to the body of the page
+document.body.appendChild( current );
 
 /**
  * Will display the specified tooltip when
@@ -14,31 +48,26 @@ var current = null;
  */
 function displayTooltip( tooltipNum )
 {
-    // An array of all the timeline tooltips
-    var tooltips = readTextFile( "scripts/Timeline.txt" ).split( "\n" );
-
-    // Create a new span element and set it to the currently viewed tooltip
-    current = document.createElement( "span" );
-
-    for ( var i = 2; i <= tooltips.length;  i += 2 )
+    // Clear the previous tooltip information
+    while ( current.firstChild )
     {
-        if ( i / 2 == tooltipNum )
-        {
-            current.appendChild( document.createElement( "h1" ).createTextNode( tooltips[ i - 2 ] ) );
-            current.appendChild( createTextNode( tooltips[ i - 1 ] ) );
-        }
+        current.removeChild( current.firstChild );
     }
 
-    // Set the class of the tooltip (for styling to take effect)
-    current.setAttribute( "class", "tooltip" );
-
-    // Add the tooltip to the body of the page
-    document.body.appendChild( current );
+    // Add the info from the tooltip we're looking for to the span
+    for ( var i = 0; i < tooltips.length;  i++ )
+    {
+        if ( i + 1 == tooltipNum )
+        {
+            current.appendChild( document.createElement( "h1" ).appendChild( document.createTextNode( tooltips[ i ].heading ) ) );
+            current.appendChild( document.createTextNode( tooltips[ i ].content ) );
+        }
+    }
 }
 
 
 // Move the selected tooltip with the mouse
-window.onmousemove = function (e)
+window.onmousemove = function ( e )
 {
     current.style.top = ( e.clientY + 20 ) + 'px';
     current.style.left = ( e.clientX + 20 ) + 'px';
