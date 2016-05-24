e549c679769cebf6b784047e675cee79d0f3ba79
carrotvegniak <veggiesrmylyfe@gmail.com>
2016-05-23 20:19:29 -0500

    Debugging organization date sorting

--- scripts/orgDisplay.js
+++ scripts/orgDisplay.js
@@ -110,12 +110,12 @@ function searchOrganizations( catType, searchFor )
 		for ( var i = itemList.length - 1; i >= 0; i-- )
 		{
 			// If the date range of the organization overlaps the selected range
-			if ( ( itemList[ i ].startDate >= parseInt( searchFor.substring( 0, searchFor.indexOf( "-" ) - 1 ) )
-				&& itemList[ i ].startDate <= parseInt( searchFor.substring( searchFor.indexOf( "-" ) + 2 ) ) )
-				|| ( itemList[ i ].endDate >= parseInt( searchFor.substring( 0, searchFor.indexOf( "-" ) - 1 ) )
-				&& itemList[ i ].endDate <= parseInt( searchFor.substring( searchFor.indexOf( "-" ) + 2 ) ) )
-				|| ( itemList[ i ].startDate <= parseInt( searchFor.substring( 0, searchFor.indexOf( "-" ) - 1 ) )
-				&& itemList[ i ].endDate >= parseInt( searchFor.substring( searchFor.indexOf( "-" ) + 2 ) ) ) )
+			if ( ( itemList[ i ].startDate >= parseInt( searchFor.substring( 0 ) )
+				&& itemList[ i ].startDate <= parseInt( searchFor.substring( 7 ) ) )
+				|| ( itemList[ i ].endDate >= parseInt( searchFor.substring( 0 )
+				&& itemList[ i ].endDate <= parseInt( searchFor.substring( 7 ) ) )
+				|| ( itemList[ i ].startDate <= parseInt( searchFor.substring( 0 ) )
+				&& itemList[ i ].endDate >= parseInt( searchFor.substring( 7 ) ) ) )
 			{
 				appendOrganization( itemList[ i ] );
 			}
